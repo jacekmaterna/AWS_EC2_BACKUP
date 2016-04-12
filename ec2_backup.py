3be4b99979ad7af4f6135a70a3ea0c25b2bd078b
J Bret Edwards <sliverman69@gmail.com>
2016-04-12 10:01:22 -0700

    Update ec2_backup.py

--- ec2_backup.py
+++ ec2_backup.py
@@ -27,13 +27,8 @@ r.close()
 region = region.getvalue()
 aws_region = region[:-1]
 
-print (instanceID)
-
 amiDescription = 'Daily Snapshot for instance ' + 'instanceID'
-
 client = boto3.client('ec2', region_name=aws_region)
-
-#get all the Volume information from the attached ebs volumes
 
 #get the list of drives that need to have their filesystems frozen
 listdrives=subprocess.Popen('df' + ' -h', shell=True, stdout=subprocess.PIPE)
@@ -46,24 +41,39 @@ subprocess.call('sync')
 
 #freeze the filesystems
 for i,mount in enumerate(listdrives.split()):
-    subprocess.check_output('sudo fsfreeze --freeze ' + mount, shell=True)
+    try:
+      subprocess.check_output('sudo fsfreeze --freeze ' + mount, shell=True)
+    except OSError as e:
+      print("mountpoint already frozen.")
+      subprocess.check_output('sudo fsfreeze --unfreeze' + mount, shell=True)
+      subprocess.check_output('sudo fsfreeze --freeze' + mount, shell=True)
+      pass
 
 #timestamp=subprocess.check_output(['/bin/date', '+"%F"'])
 timestamp=datetime.utcnow().strftime("%d %b %Y %I.%M%p")
 imageName=instanceID+'-'+timestamp
-print (imageName)
+print imageName
 
 #section to create the AMI
-response = client.create_image(
-    DryRun=False,
-    InstanceId=instanceID,
-    Name=imageName,
-    Description=amiDescription,
-    NoReboot=True
-)
+try:
+  response = client.create_image(
+      DryRun=False,
+      InstanceId=instanceID,
+      Name=imageName,
+      Description=amiDescription,
+      NoReboot=True)
+except botocore.exceptions.ClientError as e:
+  if e.response['ResponseMetadata']['HTTPStatusCode'] >= 400 :
+      print "error initiating snapshot. Response code= " + e.response['ResponseMetadata']['HTTPStatusCode'] \
+            + ".\nRequestId: " + e.response['ResponseMetadata']['RequestId']
+  pass
 
-print (response)
+print response
 
 #unfreeze the filesystems
 for i,mount in enumerate(listdrives.split()):
+  try:
     subprocess.check_output('sudo fsfreeze --unfreeze ' + mount, shell=True)
+  except OSError as e:
+    print("Error unfreezing mountpoint: " + mount + ". Appears to already be unfrozen. Snapshot dirty. DELETE: "
+          + response['ImageId'])
